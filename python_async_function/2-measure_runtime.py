f59589c43f2153ff512bc54dfe7f942ac044e4b1
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-08-26 15:46:01 +1000

    documentation'

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import time
 import asyncio
-"""Measure the runtime"""
+"""Measure the average runtime of the wait_n function"""
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
