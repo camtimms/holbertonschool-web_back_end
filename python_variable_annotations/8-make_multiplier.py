ad5cbd2b84cae316e41b3f0705d779443e1963cc
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-08-26 11:57:56 +1000

    8-make_multiplier.py

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Complex types - functions"""
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Takes a float and creates a function that multiplied a value by the
+    multiplier"""
+    def scaling_func(x: float):
+        """Function that scales by the multiplier"""
+        return x * multiplier
+    return scaling_func
