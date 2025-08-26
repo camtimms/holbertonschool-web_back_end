a148ae30436544ca2149c14b9572292427818871
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-08-26 11:07:53 +1000

    5-sum_list.py

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """Complex types - list of floats"""
 
+from typing import List
 
-def sum_list(input_list: list) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """Takes a list of floats and return their sum"""
-    sum = 0.0
+    sum: float = 0.0
     for value in input_list:
-        if type(value) is float:
-            raise TypeError("Values aren't all floats")
         sum += value
-    return (sum)
+    return sum
