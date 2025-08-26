d10638db301677de3e2be18d2537a7ea284bd72b
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-08-26 11:58:44 +1000

    8-make_multiplier.py

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -2,6 +2,7 @@
 """Complex types - functions"""
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Takes a float and creates a function that multiplied a value by the
     multiplier"""
