1416fdcf06d4161263f47f745a0cbc7ae0b56605
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-08-26 16:06:22 +1000

    documentation

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -5,4 +5,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Creates a asyncio task for wait_random"""
     return asyncio.create_task(wait_random(max_delay))
