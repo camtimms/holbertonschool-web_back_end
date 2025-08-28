9536ad6a58eaed17e27cf8d9701ed5e24568b979
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-08-28 13:04:59 +1000

    2-measure_runtime.py

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-
+"""Run time for four parallel comprehensions"""
 import asyncio
 import typing
 from time import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
+
 async def measure_runtime() -> float:
+    """Measures the time to run 4 coroutines for the async functions"""
     start_time = time()
 
     coroutines = []
