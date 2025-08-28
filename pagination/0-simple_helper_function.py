b5e808259d28aaaa2a5c22382a4563bc35e028ee
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-08-28 15:17:16 +1000

    0-simple_helper_function.py

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,7 +4,8 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple:
-    """Takes a page number and size and returns a set of indexs of that range"""
+    """Takes a page number and size and returns a set of indexs of
+    that range"""
     start = (page * page_size) - page_size
     end = page * page_size
 
