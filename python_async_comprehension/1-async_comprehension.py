5bf5351de33702a190d4bda6857970cde9365661
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-08-28 12:44:24 +1000

    1-async_comprehension.py

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Async Comprehensions"""
+async_generator = __import__('0-async_generator').async_generator
+import asyncio
+from typing import List
+
+async def async_comprehension() -> List:
+    result = [await async_generator() for num in async_generator()]
+    return result
