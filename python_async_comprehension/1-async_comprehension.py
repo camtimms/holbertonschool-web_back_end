af2883431fea9eb63a4aa829120f5e9c8a6b7b02
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-08-28 12:50:08 +1000

    documentation

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 """Async Comprehensions"""
-async_generator = __import__('0-async_generator').async_generator
 import asyncio
 from typing import List
+async_generator = __import__('0-async_generator').async_generator
 
-async def async_comprehension() -> List:
+
+async def async_comprehension() -> List[float]:
+    """Uses async comprehension to wait for async generator and returns
+    the result"""
     result = [num async for num in async_generator()]
     return result
