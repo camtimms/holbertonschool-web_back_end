997f6674b9b611fd07b97ce1e33db0f4200c5655
camtimms <cam.g.timms@gmail.com>
2025-09-01 15:43:22 +1000

    return dataview instead of type array

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -2,15 +2,15 @@ export default function createInt8TypedArray(length = 0, position = 0, value = 0
   // Create buffer
   const buffer = new ArrayBuffer(length);
 
-  // Create typed array view
-  const int8View = new Int8Array(buffer)
-
   // Check if position is valid
   if (position >= length || position < 0) {
     throw new Error('Postition outside range')
   }
 
-  int8View[position] = value;
+  // Create data view
+  const dataView = new DataView(buffer)
+
+  dataView.setInt8(position, value);
 
   return int8View;
 }
