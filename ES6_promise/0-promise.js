b00afa28b65d15eadc50420259629afecaa19915
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-09-04 14:20:33 +1000

    0-promise.js

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,12 @@
+function getResponseFromAPI() {
+    const firstPromise = new Promise((resolve, reject) => {
+        const success = true;
+    if (success) {
+        resolve("Stuff worked!");
+    }
+    else {
+        reject(Error("It broke"));
+    }
+    });
+    return firstPromise;
+}
