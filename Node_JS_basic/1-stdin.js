4c72b26cecd91865407239e8c3440e8629240af7
camtimms <cam.g.timms@gmail.com>
2025-09-08 15:56:01 +1000

    1-stdin.js

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+// Display message
+console.log("Welcome to Holberton School, what is your name?");
+
+// User imput
+process.stdin.on("data", data => {
+  const name = data.toString();
+  console.log(`Your name is ${name}`);
+})
+
+// Program exit message
+process.stdin.on("end", () => {
+  console.log("This important software is now closing");
+});
