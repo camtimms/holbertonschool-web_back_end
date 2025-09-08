1e57fb0b3f632d7b8860c39f9ec02936637beaf4
camtimms <cam.g.timms@gmail.com>
2025-09-08 16:23:12 +1000

    1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,7 +4,7 @@ console.log("Welcome to Holberton School, what is your name?");
 // User imput
 process.stdin.on("data", data => {
   const name = data.toString().trim();
-  console.log(`Your name is: ${name}\r`);
+  console.log(`Your name is: ${name}\r\n`);
 })
 
 // Program exit message
