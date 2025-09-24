730befa32fde686b917a08826c5cdbe121035ff1
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-09-24 14:50:18 +1000

    edit to use readable over data

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,10 @@
 // Display message
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
-// User input
-process.stdin.on("data", (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\r`);
-})
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  if (input !== null) process.stdout.write(`Your name is: ${input}`);
+});
 
 // Program exit message
 process.stdin.on("end", () => {
